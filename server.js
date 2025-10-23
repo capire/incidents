f36dc5956224206efa9668a1b447e98e90d4831d
D050513 <sebastian.van.syckel@sap.com>
2025-10-23 13:57:52 +0200

    assert collector

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
 require('./assert')
+
+const cds = require('@sap/cds')
+
+cds.on('compile.for.runtime', (csn, o, next) => {
+  // we need the linked model
+  const dsn = next()
+
+  function _asserts4(e) {
+    let xpr = e?.['@assert']?.xpr
+    if (!xpr) return
+    let inherited = _asserts4(e.parent.__proto__.elements?.[e.name])
+    if (inherited) {
+      const _inherited = inherited.slice(1, -1)
+      const _own = xpr.slice(1, -1)
+      // TODO: deduplication
+      xpr = ['case', ..._inherited, ..._own, 'end']
+    }
+    return xpr
+  }
+
+  for (const each in dsn.definitions) {
+    const entity = dsn.definitions[each]
+    if (entity.kind !== 'entity') continue
+
+    if (!entity.projection) continue
+
+    for (const each in entity.elements) {
+      const element = entity.elements[each]
+      if (element['@assert']) {
+        element['@assert']._xpr = element['@assert'].xpr
+        element['@assert'].xpr = _asserts4(element)
+      }
+    }
+  }
+})
--- srv/processor-service.cds
+++ srv/processor-service.cds
@@ -1,6 +1,14 @@
-using { sap.capire.incidents as my } from '../db/schema';
+using {sap.capire.incidents as my} from '../db/schema';
 
 service ProcessorService {
   entity Incidents as projection on my.Incidents;
+
+  annotate Incidents with {
+    @assert: (case
+      when title like '%important%' and urgency!='H' then 'important incidents must be high priority'
+    end)
+    title;
+  };
+
   annotate my.Customers with @cds.autoexpose;
 }
